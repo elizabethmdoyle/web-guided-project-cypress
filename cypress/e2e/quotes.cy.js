e846d12f9f8ae5631cc123c87c5d97e6d479ed67
Elizabeth Doyle <myoddjourney@gmail.com>
2023-07-06 15:09:46 -0400

    working on testing

--- cypress/e2e/quotes.cy.js
+++ cypress/e2e/quotes.cy.js
@@ -1,11 +1,10 @@
 describe("Quotes App", () => {
-    beforeEach(() => {
+    beforeEach(() => {})
     //each test needs fresh state 
     //tests shouldnt rely on other tests
     //every test should work in isolation
 
     cy.visit(`https://localhost:1234`)
-})
 
 //helpers/getters
 
@@ -22,7 +21,7 @@ it('sanity check to make sure tests work', () => {
     //There acn be multiple assertions per test but they all need 
     // to relate to the ''one thing'' that we're testing
 
-    expect(1 + 2).to.equal(3);
+    expect(i + 2).to.equal(3);
     expect(2 + 2).not.equal(5)
     expect({}).not.to.equal({}) // true === different objects
     expect({}).to.eql({}) // == type   equal is === eql is ==
@@ -75,49 +74,26 @@ describe('Filling out the inputs and cancelling', () => {
                 submitBtn().should("be.disabled")
             })
 
-    }) describe('Adding a new quote', () => {
-        it('can submit and delete a new quote', () => {
-          textInput().type('Have fun!')
-          authorInput().type('Gabe')
-          submitBtn().click()
-          // It's important that state be the samne at the beginning of each test
-          // which is why we delete the newly created post immediately.
-          // If we are not careful with this, we'll get lots of false positives and negatives.
-          // Restart the server script if necessary to go back to the original 3 quotes.
-          // Explain that in real world, a fresh testing database with predetermined data would be spun up for each test run.
-          cy.contains('Have fun!').siblings('button:nth-of-type(2)').click()
-          cy.contains('Have fun!').should('not.exist')
-        })
-    
-        it('variation of can submit a new quote', () => {
-          cy.contains(/have fun/).should('not.exist')
-          textInput().type('have fun')
-          authorInput().type('Gabe')
-          submitBtn().click()
-          cy.contains(/have fun/).should('exist')
-          cy.contains(/have fun/).next().next().click()
-          cy.contains(/have fun/).should('not.exist')
-        })
-      })
-    
-      describe('Editing an existing quote', () => {
-        it('can edit a quote', () => {
-          // Baking a new quote and submitting it.
-          textInput().type('Use Postman')
-          authorInput().type('Gabriel')
-          submitBtn().click()
-          // Hitting the edit button and checking inputs.
-          cy.contains('Use Postman').siblings('button:nth-of-type(1)').click()
-          textInput().should('have.value', 'Use Postman')
-          authorInput().should('have.value', 'Gabriel')
-          // Editing the quote and submitting changes.
-          textInput().type(' for realz')
-          authorInput().type(' Cabrejas')
-          submitBtn().click()
-          // Checking that the changes stuck.
-          cy.contains('Use Postman for realz (Gabriel Cabrejas)')
-          // Hitting the delete button for the edited quote to leave state the way it was. IMPORTANT !!
-          cy.contains('Use Postman for realz (Gabriel Cabrejas)').next().next().click()
-          cy.contains('Use Postman for realz (Gabriel Cabrejas)').should('not.exist')
-        })
-      })
+    })
+
+    describe("Adding a new quote", () => {
+       it('can submit and delete a new quote', () => {
+
+            })
+    })
+
+// it('', () => {
+
+// })
+
+
+
+
+
+
+
+
+
+
+})
+
